ad030195783a517521f57d22137de6d6e87a4c97
SigneThrane <94352955+SigneThrane@users.noreply.github.com>
2023-04-20 14:19:40 +0200

    1

--- page-stot-dyrene.php
+++ page-stot-dyrene.php
@@ -15,8 +15,27 @@ get_header(); ?>
 
 <body>
 
+<div style="width:100%; max-width:100%; margin:0px; bottom: 0px; transform: rotate(180deg);"><svg id="mymainsvgelm" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" class="" style="width: calc(124% + 1.3px); height: 64px;">
+                    <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" style="fill: rgb(93, 126, 75);"></path>
+                </svg></div>
+                <div class="foder">
+                    <div class="row">
+                        <div class="col-md-6">
+                       <br><br><br>
+                        <h3>Donér foder / strøelse / legetøj </h3>
+                        <h5>Kan din kat ikke lide det kattegrus du har købt? <br>
+                            Eller vil din hund ikke spise dens tørfoder?  <br>
+                            <br>
+                            - Så kender vi nogle der vil blive glade for det! </h5>
+                        </div>
 
-<div style="width:100%; max-width:100%; margin:0px; bottom: 0px; transform: rotate(180deg);"><svg id="mymainsvgelm" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" class="" style="width: calc(109% + 1.3px); height: 67px; transform: rotateY(180deg);">
+                        <div class="col-md-6">
+                            <br><br><br>
+                        <img  width="30%" src="<?php the_field('kat'); ?>" alt="Enghave hund" />
+                   
+                        </div>
+                    </div>
+                    <div style="width:100%; max-width:100%; margin:0px; bottom: 0px; transform: rotate(180deg);"><svg id="mymainsvgelm" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" class="" style="width: calc(109% + 1.3px); height: 67px; transform: rotateY(180deg);">
                     <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" style="fill: rgb(241, 155, 56);"></path>
                 </svg></div>
                 </div>
