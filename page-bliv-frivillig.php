957ad2b51fd5f9eb4435d50dcdd4a45e12c0e278
SigneThrane <94352955+SigneThrane@users.noreply.github.com>
2023-05-01 20:33:21 +0200

    frivillig

--- page-bliv-frivillig.php
+++ page-bliv-frivillig.php
@@ -39,10 +39,33 @@ Hvis du har yderligere spørgsmål, er du velkommen til at kontakte os via konta
 <div class="hjertet">
     <h1>Kærlighed til dyrene, er hjertet i vores arbejde</h1>
     <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-md-6"></div>
-        <div class="col-md-3"></div>
+        <div class="col-md-3">
+        <img  width="67%" src="<?php the_field('billede_1'); ?>" alt="Dyre" />
+        </div>
+        <div class="col-md-6">
+        <img  width="60%" src="<?php the_field('billede_2'); ?>" alt="Dyre" />
+        </div>
+        <div class="col-md-3">
+        <img  width="67%" src="<?php the_field('billede_3'); ?>" alt="Dyre" />
+        </div>
     </div>
+    <br><br><br><br>
+
+    <div class="wrapper">
+    <div style="width:100%; max-width:100%; margin:0px; bottom: 0px; transform: rotate(180deg);"><svg id="mymainsvgelm" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" class="" style="width: calc(117% + 1.3px); height: 73px;"><path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" style="fill: rgb(242, 232, 219);"></path></svg></div>
+</div>
+
+    <div class="rolle">
+        <h1>Din rolle som frivillig</h1>
+        <p>At være frivillig hos Dyrenes Velfærd indebærer at være en del af en organisation, der arbejder for at forbedre dyrevelfærden i samfundet. <br>
+
+Som frivillig kan man blive involveret i en række forskellige opgaver, herunder at deltage i sager, hvor man tager billeder og hjælper med at træffe <br>
+beslutninger omkring dyrenes trivsel. Man kan også være med til at pleje og passe på de dyr, der er blevet reddet, <br> og sørge for, at de får den nødvendige br behandling og omsorg. <br>
+
+Derudover kan man også hjælpe med fundraising, arrangementer og oplysningsarbejde, der bidrager til at sprede kendskabet til organisationens arbejde. <br>
+
+Som frivillig hos Dyrenes Velfærd er man en vigtig del af en indsats, der gør en positiv forskel for dyr i nød. Vi glæder os til at høre fra dig. </p>
+
 
 
 
@@ -82,15 +105,12 @@ Hvis du har yderligere spørgsmål, er du velkommen til at kontakte os via konta
 			<button>DONÉR NU </button>
 			<p>Har du spørgsmål til hvordan du kan støtte? Læs mere her</p>
 			<br>
-<<<<<<< HEAD
 
         </div>
         </div>
-        
+        </div>
 </div>
-=======
 		</div>
->>>>>>> a385a4243958724d9031785db8433072667e1664
 </body>
 
 
